2c0c32e73559d9433f1a4aeed6a1ae96b58a2be4
RPSJR <raimundopsjr@gmail.com>
2026-03-16 20:56:26 -0300

    [FIX] l10n_br_base_address: substitui API dos Correios pelo Viacep

--- l10n_br_base_address/models/zip_search.py
+++ l10n_br_base_address/models/zip_search.py
@@ -4,10 +4,7 @@ from odoo import models
 
 _logger = logging.getLogger(__name__)
 
-try:
-    from zeep import Client
-except ImportError:
-    _logger.error("Cannot import zeep library", exc_info=True)
+
 
 
 class ZipSearchMixin(models.AbstractModel):
@@ -16,23 +13,29 @@ class ZipSearchMixin(models.AbstractModel):
 
     def search_address_by_zip(self, zip_code):
         zip_code = re.sub('[^0-9]', '', zip_code or '')
-        client = Client('https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl') # noqa
         try:
-            res = client.service.consultaCEP(zip_code)
-        except:
+            import requests
+            url = f"https://viacep.com.br/ws/{zip_code}/json/"
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+            res = response.json()
+            if "erro" in res:
+                return {}
+        except Exception:
             return {}
+
         state = self.env['res.country.state'].search(
             [('country_id.code', '=', 'BR'),
-             ('code', '=', res['uf'])])
+             ('code', '=', res.get('uf'))])
 
         city = self.env['res.city'].search([
-            ('name', '=ilike', res['cidade']),
+            ('name', '=ilike', res.get('localidade')),
             ('state_id', '=', state.id)])
 
         return {
             'zip': zip_code,
-            'street': res['end'],
-            'l10n_br_district': res['bairro'],
+            'street': res.get('logradouro'),
+            'l10n_br_district': res.get('bairro'),
             'country_id': state.country_id.id,
             'state_id': state.id,
             'city_id': city.id
